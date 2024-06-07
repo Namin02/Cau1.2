6ecb043cff17bafb9e0894a43d8f804e6c44b7cf
Naminee <128821801+Namin02@users.noreply.github.com>
2024-06-07 10:48:21 +0700

    asdfvg
    sdfvgbn

---
+++ BT2-cau2.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import time
+import webbrowser as wb
+
+st.title('Đăng nhập')
+username = st.text_input('Username', '')
+password = st.text_input('Password', '', type='password')
+if st.button('Đăng nhập'):
+    if username == '21522698' and password == '2003':
+        st.success('Đăng nhập thành công')
+        time.sleep(3)
+        wb.open('https://www.uit.edu.vn/')
+    else:
+        st.error('Sai username hoặc password')
+    
